519e39dca18e885c7d834163ea29b14f48c9c3ef
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-11-19 15:50:48 +0530

    trailer fixed

--- src/Components/Watch.jsx
+++ src/Components/Watch.jsx
@@ -25,11 +25,15 @@ const Watch = () => {
 
   return (
     <div className=''>
-      <iframe className='w-full h-screen aspect-video'
-       src={"https://www.youtube.com/embed/"+key+"?&autoplay=1"}
-        title="YouTube video player"
-         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        ></iframe>
+        <iframe
+    className="w-full h-screen aspect-video"
+    src={"https://www.youtube.com/embed/"+key+"?&autoplay=1"}
+    title="YouTube video player"
+    frameBorder="0" // Correct casing
+    referrerPolicy="strict-origin-when-cross-origin" // Correct casing
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowFullScreen // Correct casing
+  ></iframe>
     </div>
   )
 }
